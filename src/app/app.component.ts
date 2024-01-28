c3433668dca897dcf781632b827b318c622a288e
nikkinicholasromero <nikkinicholas.romero@gmail.com>
2024-01-29 04:00:26 +0800

    Added tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  `
 })
 export class AppComponent {
-  title = 'demo';
 }
